2c6470878aa3594666fc129d238afe11846791c9
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-12-08 11:30:17 +0200

    replaced the Recorder.html given code by the html page with aonther  and added a controller MVC empty tamplate which is called AudioController.cs

---
+++ StudioRecorderMVC/Controllers/AudioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudioRecorderMVC.Controllers
+{
+    public class AudioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
