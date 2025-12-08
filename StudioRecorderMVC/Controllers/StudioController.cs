2ace1efdbc1be90cef52b02139bf662be775510a
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-12-08 15:36:51 +0200

    creating a studiocontroller adding a background colour for home page and index page and updating layout to correspond  , adding a  master effect for the voice by updating the recorder.html

---
+++ StudioRecorderMVC/Controllers/StudioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudioRecorderMVC.Controllers
+{
+    public class StudioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
