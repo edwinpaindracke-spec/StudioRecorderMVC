a75dcbd37c514b5843064c26cdf4b652715d8571
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-12-08 11:15:48 +0200

    Add project files.

---
+++ StudioRecorderMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudioRecorderMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
