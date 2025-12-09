ac2c70c13ee2b40dd6b070fc224dce6b414788d4
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-12-09 09:45:43 +0200

    adding a function of High-Quality Recording,Built-in Editing Tools,Cloud Read pages by adding a Razor view pages ,added a controller and a folderto hold the pages

---
+++ StudioRecorderMVC/Controllers/FeaturesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudioRecorderMVC.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        public IActionResult Recording()
+        {
+            return View();
+        }
+
+        public IActionResult Editing()
+        {
+            return View();
+        }
+
+        public IActionResult Cloud()
+        {
+            return View();
+        }
+    }
+}
