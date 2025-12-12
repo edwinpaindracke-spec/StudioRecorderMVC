ef6577121cabdde0ae4e0797553cb83625463801
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-12-12 15:26:33 +0200

    adding a button for each categories for cuting,volume,mix, and export so users can be able to do it in their own

--- StudioRecorderMVC/Controllers/EditController.cs
+++ StudioRecorderMVC/Controllers/EditController.cs
@@ -1,9 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace YourProject.Controllers
 {
     public class EditController : Controller
     {
+        public ActionResult Hub() { return View(); }
         public ActionResult Cut() { return View(); }
         public ActionResult Volume() { return View(); }
         public ActionResult Mix() { return View(); }
